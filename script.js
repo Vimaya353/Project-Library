b9b4d29703d67df115bbbeb5b171463eb9d1bcc0
Vimaya <arionlester88@gmail.com>
2026-09-16 10:37:49 +0530

    display books as library cards

--- script.js
+++ script.js
@@ -16,3 +16,25 @@ function addBookToLibrary(title, author, pages, read) {
 addBookToLibrary("The Hobbit","J.R.R. Tolkien", 310, true);
 addBookToLibrary("1984","George Orwell", 328, false);
 addBookToLibrary("To Kill a Mockingbird","Harper Lee", 281, true);
+
+function displayBooks(){
+    const library = document.querySelector("#library");
+
+    library.innerHTML = "";
+
+    myLibrary.forEach(book => {
+        const bookElement = document.createElement("div");
+
+        bookCard.classList.add("book-card");
+
+        bookCard.innerHTML = `
+            <h2>${book.title}</h2>
+            <p>Author: ${book.author}</p>
+            <p>Pages: ${book.pages}</p>
+            <p>${book.read ? "Read" : "Not Read"}</p>
+        `;
+        library.appendChild(bookElement);
+    });
+}
+
+displayBooks();
